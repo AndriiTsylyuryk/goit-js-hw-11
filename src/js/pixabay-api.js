9c6723d5d4bdb0437d4428649bf784b3c179a204
AndriiTsylyuryk <154674406+AndriiTsylyuryk@users.noreply.github.com>
2024-05-25 17:32:55 +0300

    in progress

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,9 +1,9 @@
 
 
 
- export function searchImg(img) {
+ export function getImages (img) {
     const BASE_URL = 'https://pixabay.com/api/'
-    // const END_POINT = ;
+    
     const params = new URLSearchParams({
         key: '44051747-21f0728748ba5e469c568f2fa',
         q: img,
@@ -15,5 +15,5 @@
 
     const url = `${BASE_URL}?${params}`;
 
-    return fetch(url).then(res => console.log(res.json()));
+    return fetch(url).then(res => res.json());
 }
--- src/main.js
+++ src/main.js
@@ -1,17 +1,56 @@
 import './js/pixabay-api';
-import { searchImg } from "./js/pixabay-api";
+import { getImages  } from './js/pixabay-api';
+import { imagesRender } from './js/render-functions';
 
 
-const form = document.querySelector('.search'); // Вибираємо форму
+
+import iziToast from 'izitoast';
+
+import 'izitoast/dist/css/iziToast.min.css';
+
+
+
+const form = document.querySelector('.search');
 const input = document.querySelector('.input');
-form.addEventListener('submit', handleSubmit); // Додаємо обробник події до форми
+const container = document.querySelector('.pictures');
+
+form.addEventListener('submit', handleSubmit);
 
 function handleSubmit(event) {
-    event.preventDefault();
-    if (!input.value) {
-        alert('alert')
-    }
+  event.preventDefault();
 
-    searchImg(input.value);
-    
+  container.innerHTML = '';
+
+  const query = event.target.elements.field.value.trim();
+  console.log(query)
+
+  if (!query) {
+    iziToast.error({
+      title: 'Error',
+      message:
+        '"Sorry, there are no images matching your search query. Please try again!"',
+      position: 'topRight',
+    });
+    return;
+  }
+
+  getImages(query).then(data => {
+    console.log(data)
+    if (data) {
+      imagesRender(data.hits);
+    } else {
+      throw Error(
+        'Sorry, there are no images matching your search query. Please try again!'
+      );
+    }
+  }).catch(error => {
+    iziToast.error({
+      message:
+        'Sorry, there are no images matching your search query. Please try again!',
+      position: 'topRight',
+    });
+  })
+ 
+  
 }
+
