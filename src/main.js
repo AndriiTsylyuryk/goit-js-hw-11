27b83a12f618a26c04617924587fe28f59aff50b
AndriiTsylyuryk <154674406+AndriiTsylyuryk@users.noreply.github.com>
2024-05-26 11:16:25 +0300

    save

--- src/main.js
+++ src/main.js
@@ -1,28 +1,23 @@
 import './js/pixabay-api';
-import { getImages  } from './js/pixabay-api';
+import { getImages } from './js/pixabay-api';
 import { imagesRender } from './js/render-functions';
-
-
 
 import iziToast from 'izitoast';
 
 import 'izitoast/dist/css/iziToast.min.css';
 
-
-
 const form = document.querySelector('.search');
-const input = document.querySelector('.input');
+// const input = document.querySelector('.input');
 const container = document.querySelector('.pictures');
+const loader = document.querySelector('.loader');
 
 form.addEventListener('submit', handleSubmit);
 
 function handleSubmit(event) {
   event.preventDefault();
-
   container.innerHTML = '';
 
   const query = event.target.elements.field.value.trim();
-  console.log(query)
 
   if (!query) {
     iziToast.error({
@@ -34,23 +29,37 @@ function handleSubmit(event) {
     return;
   }
 
-  getImages(query).then(data => {
-    console.log(data)
-    if (data) {
-      imagesRender(data.hits);
-    } else {
-      throw Error(
-        'Sorry, there are no images matching your search query. Please try again!'
-      );
-    }
-  }).catch(error => {
-    iziToast.error({
-      message:
-        'Sorry, there are no images matching your search query. Please try again!',
-      position: 'topRight',
+  showLoader();
+
+  getImages(query)
+    .then(data => {
+      if (data) {
+        if (data.hits.length === 0) {
+          throw Error(
+            'Sorry, there are no images matching your search query. Please try again!'
+          );
+        }
+        imagesRender(data.hits);
+      }
+    })
+    .catch(error => {
+      iziToast.error({
+        message:
+          'Sorry, there are no images matching your search query. Please try again!',
+        position: 'topRight',
+      });
+    })
+    .finally(() => {
+      hideLoader();
+      form.reset();
     });
-  })
- 
-  
 }
 
+
+function showLoader() {
+  loader.style.display = 'inline-block';
+}
+
+function hideLoader() {
+  loader.style.display = 'none';
+}
